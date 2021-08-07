e91ee38981f97c36a5ee6d58133b1ccbeefe350a
philip howley <howley.phil@gmail.com>
2021-08-07 11:02:11 -0400

    Add events review

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-// Example 1
-// We've seen a few ways to define functions:
-// slightly different but largely function similarly
+const myGreetBtn = document.querySelector('.btn-greet');
+// console.log(myGreetBtn);
+function myFunction() {
+  document.getElementById('demo').innerHTML = 'Hello World';
+}
 
-// function foo() {
-//   console.log('foo');
-// }
+const cats = ['meowzer', 'catzilla'];
+for (let i = 0; i < 10; i++) {
+  console.log(cats[i]);
+}
 
-// var bar = function () {
-//   console.log('bar');
-// };
-
-// const baz = function () {
-//   console.log('baz');
-// };
-
-// Example 2
-
-// Now we're going to learn a short-hand that has one glaring and very important difference. This is called an arrow function.
-
-// Let's convert this add function:
-
-// Start code
-// const add = (num1, num2) => num1 + num2;
-// console.log(add(2,2)); // 4
-// First we replace the function keyword with an arrow:
-
-// const add = (num1, num2) => {
-//   return num1 + num2;
-// };
-
-// Example 3 - Refactor even more
-// If our function just has one line of logic that we want to return we can eliminate the return keyword and curly braces!
-// const add = num1 => num1 + num2;
-
-// Next
-// $ git checkout 08-arrow-func-activity-start
+document.addEventListener('click', myFunction);
