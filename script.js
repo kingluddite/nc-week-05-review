3d4e57c6c47540d819978ad6131f885fef2ee457
philip howley <howley.phil@gmail.com>
2021-08-07 09:22:42 -0400

    basic function

---
+++ script.js
@@ -0,0 +1,11 @@
+function greet(name, age) {
+  // stuff to do
+  console.log(`hello ${name}. I have ${age} years of development experience`);
+}
+
+greet('zack', 2);
+greet('samantha', 2);
+greet('john', 3);
+greet('spencer', 10);
+greet('daniel', 20);
+greet('phil', 1);
